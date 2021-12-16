2d0b182c98c15ffd40423e331af25c1908df33e8
doooooori <90612060+doooooori@users.noreply.github.com>
2021-12-16 16:12:19 +0700

    Update ParseWorkers.cpp

--- ParseWorkers.cpp
+++ ParseWorkers.cpp
@@ -6,137 +6,145 @@
 #include "Operators.cpp"
 #include "ParseWorkers.h"
 #include <memory>
-void create_worker(const std::string& name,std::map<int,std::vector<std::string>> blocks,int number, std::vector<std::shared_ptr<Worker>> &workers) {
+
+using namespace std;
+
+void create_worker(const string& name, map<int, vector<string>> blocks,int number, vector<shared_ptr<Worker>> &workers) {
     if(name=="readfile"){
         if(blocks[number].size()==4) {
-            auto Operator = std::make_shared<Readfile>(blocks[number][3]);
+            auto Operator = make_shared<Readfile>(blocks[number][3]);
             workers.push_back(Operator);
         }
-        else throw std::string("readfile arguments is wrong\n");
+        else throw runtime_error("Readfile arguments is wrong\n");
 
     }
     else if(name=="writefile"){
         if(blocks[number].size()==4) {
-            auto Operator = std::make_shared<Writefile>(blocks[number][3]);
+            auto Operator = make_shared<Writefile>(blocks[number][3]);
             workers.push_back(Operator);
         }
-        else throw std::string("writefile arguments is wrong\n");
+        else throw runtime_error("Writefile arguments is wrong\n");
     }
     else if(name=="sort"){
-        auto Operator = std::make_shared<Sort>();
+        auto Operator = make_shared<Sort>();
         workers.push_back(Operator);
     }
     else if(name=="grep"){
         if(blocks[number].size()==5) {
-            auto Operator = std::make_shared<Grep>(blocks[number][4]);
+            auto Operator = make_shared<Grep>(blocks[number][4]);
             workers.push_back(Operator);
         }
-        else throw std::string("grep arguments is wrong\n");
+        else throw runtime_error("Grep arguments is wrong\n");
     }
     else if(name=="replace"){
         if(blocks[number].size()==5) {
-            auto Operator = std::make_shared<Replace>(blocks[number][3], blocks[number][4]);
+            auto Operator = make_shared<Replace>(blocks[number][3], blocks[number][4]);
             workers.push_back(Operator);
         }
-        else throw std::string("replace arguments is wrong\n");
+        else throw runtime_error("Replace arguments is wrong\n");
     }
     else if(name=="dump"){
         if(blocks[number].size()==4) {
-            auto Operator = std::make_shared<Dump>(blocks[number][3]);
+            auto Operator = make_shared<Dump>(blocks[number][3]);
             workers.push_back(Operator);
         }
-        else throw std::string("dump arguments is wrong\n");
+        else throw runtime_error("Dump arguments is wrong\n");
     }
-    else throw name + std::string(" realization not found");
+    else throw name + string(" realization not found");
 
 }
-std::vector<std::shared_ptr<Worker>> ParseWorkers (const std::string& filename,std::map<int,std::vector<std::string>> blocks){
-    std::vector<std::shared_ptr<Worker>> workers;
-    std::ifstream file (filename);
-    std::string line;
+vector<shared_ptr<Worker>> ParseWorkers (const string& filename,map<int,vector<string>> blocks){
+    vector<shared_ptr<Worker>> workers;
+    ifstream file (filename);
+    string line;
+    int flag = 0;
+    int flag_sub = 0;
     if(file.is_open()){
-        bool flag_desc=false,flag_csed=false;
+        bool flag_desc = false,flag_csed = false;
         while(getline(file,line)){
 
-            if(line=="desc"){
-                flag_desc=true;
+            if(line == "desc"){
+                flag_desc = true;
                 continue;
             }
-            if(line=="csed"){
-                flag_csed=true;
+            if(line == "csed"){
+                flag_csed = true;
                 continue;
             }
 
             if(flag_desc && !flag_csed){
                 try{
-                    if(line.size()<3){
+                    if(line.size() < 3){
                         throw -1;
                     }
                 }
                 catch (int ex) {
                     if(line.empty()){
-                        throw std::string("Line in \"") + filename + std::string("\" is empty:\n");
+                        throw string("Line in \"") + filename + string("\" is empty:\n");
                     }
-                    throw std::string("This line in \"") + filename + std::string("\" is wrong:\n")+line;
+                    throw string("This line in \"") + filename + string("\" is wrong:\n") + line;
                 }
-                std::istringstream ss(line);
-                std::istream_iterator<std::string> begin(ss), end;
+                istringstream ss(line);
+                istream_iterator<string> begin(ss), end;
 
-                //putting all the tokens in the vector
-                std::vector<std::string> arrayTokens(begin, end);
+                vector<string> arrayTokens(begin, end);
                 int number;
                 try{
 
-                    number=std::stoi(arrayTokens[0]);
+                    number=stoi(arrayTokens[0]);
                 }
 
-                catch(const std::exception&  ex){
-                    throw std::string("This line in \"") + filename + std::string("\" is wrong:\n") + line;
+                catch(const exception&  ex){
+                    throw string("This line in \"") + filename + string("\" is wrong:\n") + line;
                 }
                 auto search = blocks.find(number);
                 if (search != blocks.end()) {
-                    throw std::string("This line in \"") + filename + std::string("\" is wrong(redefinition operator):\n") + line;
+                    throw string("This line in \"") + filename + string("\" is wrong(redefinition operator):\n") + line;
                 } else {
                     blocks[number]=arrayTokens;
                 }
-
-
+                flag = 1;
+            }
+            else if(!flag_desc && flag != 1){
+                throw runtime_error("Desc is missing");
             }
             else if(flag_csed){
-                //take number
-                //Who i'm (Operator)
-                //Create Operator with args
                 std::istringstream ss(line);
-                std::istream_iterator<std::string> begin(ss), end;
-                //putting all the tokens in the vector
-                std::vector<std::string> arrayTokens(begin, end);
+                std::istream_iterator<string> begin(ss), end;
+
+                vector<string> arrayTokens(begin, end);
                 for(int i=0;i<arrayTokens.size();i+=2){
                     int number;
                     try{
-                        number=std::stoi(arrayTokens[i]);
+                        number=stoi(arrayTokens[i]);
                     }
-                    catch(const std::exception&  ex){
-                        throw std::string("This line in \"") + filename + std::string("\" is wrong:\n") + line;
+                    catch(const exception&  ex){
+                        throw string("This line in \"") + filename + string("\" is wrong:\n") + line;
                     }
-                    //take number
                     auto search = blocks.find(number);
                     if (search != blocks.end()) {
-                        std::string operator_name = blocks[number][2];// Operator name by key
-                        create_worker( operator_name,blocks, number,workers);// Create Object_Operator with data
-                    } else {
-                        throw std::string("Operator under the number ") + arrayTokens[i] +std::string(" not found");
+                        string operator_name = blocks[number][2];
+                        create_worker( operator_name,blocks, number,workers);
                     }
-
-
+                    else {
+                        throw string("Operator under the number ") + arrayTokens[i] +string(" not found");
+                    }
+                    flag_sub = 1;
                 }
             }
             else {
-                throw std::string("This line in \"") + filename + std::string("\" is wrong:\n") + line;
+                throw string("This line in \"") + filename + string("\" is wrong:\n") + line;
             }
         }
+        if(!flag_csed){
+            throw runtime_error("Csed is missing");
+        }
+        else if(flag_sub != 1){
+            throw runtime_error("Subsequence is missing!");
+        }
     }
-    else{
-        throw std::string("File is not open!");
+    else {
+        throw runtime_error("File is not open!");
     }
     file.close();
     return workers;
