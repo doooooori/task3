a59bb9563cad0452ac506537279125fbbf323fad
doooooori <90612060+doooooori@users.noreply.github.com>
2021-12-16 16:11:06 +0700

    Update Operators.h

--- Operators.h
+++ Operators.h
@@ -1,17 +1,19 @@
 #include <string>
+#include <utility>
 #include <vector>
 #include "Worker.h"
 #include <algorithm>
 #ifndef TASK_33_OPERATORS_H
 #define TASK_33_OPERATORS_H
 
+using namespace std;
 
 class Readfile: public Worker{
 private:
-    std::string in_file_name;
+    string in_file_name;
 public:
-    explicit Readfile(std::string file_name):in_file_name(file_name){};
-    virtual void run(std::vector<std::vector<std::string>> &in,std::vector<std::vector<std::string>> &out);
+    explicit Readfile(string file_name):in_file_name(move(file_name)){};
+    void run(vector<vector<string>> &in,vector<vector<string>> &out) override;
 };
 
 
@@ -19,46 +21,44 @@ class Writefile: public Worker{
 private:
     std::string out_file_name;
 public:
-    explicit Writefile(std::string file_name):out_file_name(file_name){};
-    virtual void run(std::vector<std::vector<std::string>> &in,std::vector<std::vector<std::string>> &out);
+    explicit Writefile(string file_name):out_file_name(move(file_name)){};
+    void run(vector<vector<string>> &in,vector<vector<string>> &out) override;
 };
 
 
 class Grep: public Worker{
 private:
-    std::string word;
+    string word;
 public:
-    explicit Grep(std::string word) : word(word) {};
-    virtual void run(std::vector<std::vector<std::string>> &in,std::vector<std::vector<std::string>> &out);
+    explicit Grep(string word) : word(move(word)) {};
+    void run(vector<vector<string>> &in,vector<vector<string>> &out) override;
 }; //save string only with word
 
 
 class Sort: public Worker{
 public:
-    explicit Sort()= default;;
-    virtual void run(std::vector<std::vector<std::string>> &in,std::vector<std::vector<std::string>> &out);
+    explicit Sort()= default;
+    void run(vector<vector<string>> &in,vector<vector<string>> &out) override;
 }; //sort tokens
 
 
 class Replace: public Worker{
 private:
-    std::string word_1;
-    std::string word_2;
+    string word_1;
+    string word_2;
 public:
-    explicit Replace(std::string word1,std::string word2) : word_1(word1),word_2(word2) {};
-    virtual void run(std::vector<std::vector<std::string>> &in,std::vector<std::vector<std::string>> &out);
+    explicit Replace(string word1,string word2) : word_1(move(word1)),word_2(move(word2)) {};
+    void run(vector<vector<string>> &in,vector<vector<string>> &out) override;
 };
 
 
 class Dump: public Worker{
 private:
-    std::string out_file_name;
+    string out_file_name;
 public:
-    explicit Dump(std::string file_name):out_file_name(file_name){};
-    virtual void run(std::vector<std::vector<std::string>> &in,std::vector<std::vector<std::string>> &out);
+    explicit Dump(string file_name):out_file_name(move(file_name)){};
+    void run(vector<vector<string>> &in,vector<vector<string>> &out) override;
 };
-
-
 
 
 #endif //TASK_33_OPERATORS_H
