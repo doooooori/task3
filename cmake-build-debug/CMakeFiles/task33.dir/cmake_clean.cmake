19cbe7eb95e0a536c8f29eef89b493513066a1d6
doooooori <90612060+doooooori@users.noreply.github.com>
2021-12-16 16:23:26 +0700

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/task33.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task33.dir/main.cpp.obj"
+  "libtask33.dll.a"
+  "task33.exe"
+  "task33.exe.manifest"
+  "task33.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task33.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
